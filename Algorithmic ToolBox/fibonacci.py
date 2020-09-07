165cc7d250600e58bb592e6cac8e3c827e3954e2
rishittripathi-therocking <rishittripathi13@gmail.com>
2020-09-07 11:49:25 +0530

    Fibnacci Number

--- Algorithmic ToolBox/fibonacci.py
+++ Algorithmic ToolBox/fibonacci.py
@@ -3,7 +3,17 @@ def calc_fib(n):
     if (n <= 1):
         return n
         
-    return calc_fib(n - 1) + calc_fib(n - 2)
+    else:
+        l=[0 for i in range(0,n)]
+        l[0]=1
+        l[1]=1
+        if n==2:
+            return l[0]+l[1]
+        else:
+            for i in range(2,n):
+                l[i]=l[i-1]+l[i-2]
+            return l[n-1]
+            
 
 n = int(input())
 print(calc_fib(n))
