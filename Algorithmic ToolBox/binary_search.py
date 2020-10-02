72c324955d075c50345582092ff751f2740cabb5
rishittripathi-therocking <rishittripathi13@gmail.com>
2020-10-02 11:55:26 +0530

    binary Search

--- Algorithmic ToolBox/binary_search.py
+++ Algorithmic ToolBox/binary_search.py
@@ -2,15 +2,16 @@
 import sys
 
 def binary_search(a, x, l, r):
-    left, right = l,r
-    # write your code here
-    mid=(l+r)//2
-    if a[mid]==x:
-        return mid
-    elif x<a[mid]:
-        binary_search(a,x,0,mid-1)
-    else:
-        binary_search(a,x,mid+1, len(a))
+    while l <= r: 
+  
+        mid = l + (r - l) // 2; 
+        if a[mid] == x: 
+            return mid 
+        elif a[mid] < x: 
+            l = mid + 1
+        else: 
+            r = mid - 1
+    return -1
 
 def linear_search(a, x):
     for i in range(len(a)):
@@ -18,10 +19,11 @@ def linear_search(a, x):
             return i
     return -1
 
-data = list(map(int, input.split()))
-n = data[0]
-m = data[n + 1]
-a = data[1 : n + 1]
-for x in data[n + 2:]:
+a=list(map(int,input().split()))
+b=list(map(int,input().split()))
+n=a[0]
+arr=a[1:n+1]
+
+for x in b[1:]:
     # replace with the call to binary_search when implemented
-    print(binary_search(a, x, 0, len(a)), end = ' ')
+    print(binary_search(arr, x, 0, len(arr)-1), end = ' ')
