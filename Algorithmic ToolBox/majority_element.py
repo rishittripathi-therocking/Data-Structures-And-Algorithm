ae02f2722ed68ad57ac8d00ee19d4ec1583f81f2
rishittripathi-therocking <rishittripathi13@gmail.com>
2020-10-02 12:23:18 +0530

    majority_element

--- Algorithmic ToolBox/majority_element.py
+++ Algorithmic ToolBox/majority_element.py
@@ -1,22 +1,27 @@
-# Uses python3
-import sys
-
-def get_majority_element(a, left, right):
-    a.sort()
-    cnt=1
-    for i in range(1,right):
-        if a[i-1]!=a[i]:
-            if cnt>right/2:
-                return True
-            cnt=0
-        else:
-            cnt+=1
-    return False
+n = int(input())
+seq = [int(i) for i in input().split()]
 
 
-n=int(input())
-a = list(map(int, input().split()))
-if get_majority_element(a, 0, n) :
-    print(1)
-else:
-    print(0)
+def divide_func(seq, l, r):
+    if l+1==r:
+        return seq[l]
+    elif l+2==r:
+        return seq[l]
+    m = (l+r)//2
+    left = divide_func(seq, l, m)
+    right = divide_func(seq, m, r)
+
+    c1, c2 = 0, 0
+    for i in seq[l:r]:
+        if i == left:
+            c1+=1
+        elif i == right:
+            c2+=1
+    if c1>(r-l)//2 and left != -1:
+        return left
+    elif c2>(r-l)//2 and right != -1:
+        return right
+    else: 
+        return -1
+
+print(int(divide_func(seq, 0, n) != -1))
