2d90d14434701aaea2513be7e6ed95fcf656ed5f
rishittripathi-therocking <rishittripathi13@gmail.com>
2020-10-02 12:33:32 +0530

    Sorting

--- Algorithmic ToolBox/sorting.py
+++ Algorithmic ToolBox/sorting.py
@@ -1,48 +1,53 @@
+import sys
 import random
-import time
-def quicksort3(arr, l, r):
-    # print('Splitting:', arr[l:r])
-    if l + 1 >= r:
-        return
 
-    # Pivot selection; Return a random integer N such that l <= N <= r
-    m = random.randint(l, r-1)
-    # temp = sorted([(0,arr[0]), ((l+r)//2,arr[(l+r)//2]), (-1,arr[-1])], key = lambda x: x[1])
-    # m = temp[1][0]
-    arr[l], arr[m] = arr[m], arr[l]
-
-    # partition procedure
-    m1, m2 = partition3(arr, l, r)
-
-    quicksort3(arr, l, m1)
-    quicksort3(arr, m2+1, r)
-
-def partition3(arr, l, r):
-    m2 = l
-    for i in range(l+1, r):
-        if arr[i] <= arr[l]:
-            arr[m2+1], arr[i] = arr[i], arr[m2+1]
-            m2 += 1
-
-    arr[l], arr[m2] = arr[m2], arr[l]
-
+def partition3(a, l, r):
+    #write your code here
+    x = a[l]
     m1 = l
-    for i in range(l, m2):
-        if arr[i] < arr[m2]:
-            arr[i], arr[m1] = arr[m1], arr[i]
-            m1 += 1
-    return m1, m2
+    m2 = l
+    for i in range(l + 1, r + 1):
+    	if a[i] < x:
+        	a[i],a[m1] = a[m1],a[i]
+        	m1+=1
+        	m2+=1
+        	a[i],a[m2] = a[m2],a[i]
+    	elif a[i] == x:
+        	m2+=1
+        	a[i],a[m2] = a[m2],a[i]
+   # a[l],a[m1] = a[m1],a[l]
+    return m1,m2    	    	
 
-def create_array(size):
-    return [random.choice(list(range(10))) for _ in range(size)]
 
-# n = int(input())
-# seq = [int(i) for i in input().split()]
-# for x in seq:
-    # print(x, end=' ')
 
-t1 = time.time()
-seq = create_array(100000)
-quicksort3(seq, 0, 100000)
-t2 = time.time()
-print('Time taken:', t2-t1)
+
+
+
+def partition2(a, l, r):
+    x = a[l]
+    j = l
+    for i in range(l + 1, r + 1):
+        if a[i] <= x:
+            j += 1
+            a[i], a[j] = a[j], a[i]
+    a[l], a[j] = a[j], a[l]
+    return j
+
+
+def randomized_quick_sort(a, l, r):
+    if l >= r:
+        return
+    k = random.randint(l, r)
+    a[l], a[k] = a[k], a[l]
+    #use partition3
+    j,k = partition3(a, l, r)
+    randomized_quick_sort(a, l, j-1);
+    randomized_quick_sort(a, k+1, r);
+
+
+if __name__ == '__main__':
+    input = sys.stdin.read()
+    n, *a = list(map(int, input.split()))
+    randomized_quick_sort(a, 0, n - 1)
+    for x in a:
+        print(x, end=' ')
