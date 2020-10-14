c6d88cbcb5340b7d7be565e892f32335511bea19
rishittripathi-therocking <rishittripathi13@gmail.com>
2020-10-14 11:49:10 +0530

    Updating All

--- Algorithmic ToolBox/partition3.py
+++ Algorithmic ToolBox/partition3.py
@@ -2,19 +2,37 @@
 import sys
 import itertools
 
-def partition3(A):
-    for c in itertools.product(range(3), repeat=len(A)):
-        sums = [None] * 3
-        for i in range(3):
-            sums[i] = sum(A[k] for k in range(len(A)) if c[k] == i)
+def partition3(A, n, sum, p):
+    sum1 = [[0 for i in range(sum+1)] for j in range(n+1)]
+    for i in range(0, n+1):
+        sum1[i][0] = (i+1)*n
 
-        if sums[0] == sums[1] and sums[1] == sums[2]:
-            return 1
+    for i in range(1, n+1):
+        for j in range(1, sum+1):
+            if sum1[i-1][j] >0 and j != sum:
+                sum1[i][j] = sum1[i-1][j]
+            if A[i-1] <= j:
+                if sum1[i-1][j-A[i-1]] > 0:
+                    sum1[i][j] = sum1[i-1][j-A[i-1]] + 1
 
-    return 0
+    diff = 0
+    for i in range(1, n+1):
+        if sum1[i][sum] != sum1[i-1][sum]:
+            diff += 1
+    if diff >= 3:
+        return 1
+    else:
+        return 0
+
 
 if __name__ == '__main__':
     input = sys.stdin.read()
     n, *A = list(map(int, input.split()))
-    print(partition3(A))
-
+    sum = 0
+    for i in A:
+        sum += i
+    if sum % 3 == 0:
+        sum = int(sum / 3)
+        print(partition3(A, n, sum, 1))
+    else:
+        print('0')
